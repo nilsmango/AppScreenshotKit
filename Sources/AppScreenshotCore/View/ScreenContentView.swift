196e7d63825da6fa6e0fd671af27c0b20a8d8552
nilsmango <simonmilagrolang@icloud.com>
2026-03-28 22:42:07 +0100

    Remove NavigationBarMarginFix (navigation title padding attempt that didn't work)

--- Sources/AppScreenshotCore/View/ScreenContentView.swift
+++ Sources/AppScreenshotCore/View/ScreenContentView.swift
@@ -22,10 +22,6 @@ struct ScreenContentView<Content: View>: View {
         HostingViewWrap {
             content
         }
-        .background {
-            NavigationBarMarginFix(edgePadding: screenEdgePadding)
-                .allowsHitTesting(false)
-        }
         .frame(width: model.screenSize.width, height: model.screenSize.height)
         .overlay(alignment: .top) {
             if statusBarShown {
@@ -74,75 +70,4 @@ struct ScreenContentView<Content: View>: View {
             )
         )
     }
-
-    private var screenEdgePadding: CGFloat {
-        // iPhones with Dynamic Island need horizontal padding for the large
-        // navigation title; iPads and older iPhones without it don't.
-        model.dynamicIdsand != nil ? 16 : 0
-    }
 }
-
-#if canImport(UIKit)
-    /// Finds the UINavigationBar in the ancestor view hierarchy and sets its layout margins
-    /// so large navigation titles get proper horizontal padding.
-    private struct NavigationBarMarginFix: UIViewControllerRepresentable {
-        let edgePadding: CGFloat
-
-        func makeUIViewController(context: Context) -> UIViewController {
-            let vc = UIViewController()
-            vc.view.backgroundColor = .clear
-            vc.view.isUserInteractionEnabled = false
-            return vc
-        }
-
-        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
-            guard let navBar = findNavigationBar(from: uiViewController) else { return }
-            navBar.layoutMargins = UIEdgeInsets(
-                top: 0,
-                left: edgePadding,
-                bottom: 0,
-                right: edgePadding
-            )
-        }
-
-        private func findNavigationBar(from viewController: UIViewController) -> UINavigationBar? {
-            // Check parent hierarchy
-            var current = viewController.parent
-            while let parent = current {
-                if let nav = parent as? UINavigationController {
-                    return nav.navigationBar
-                }
-                for child in parent.children {
-                    if let nav = child as? UINavigationController {
-                        return nav.navigationBar
-                    }
-                }
-                current = parent
-            }
-            // Check sibling/ancestor view hierarchy
-            var view: UIView? = viewController.view.superview
-            while view != nil {
-                if let navBar = findNavBarInHierarchy(view!) {
-                    return navBar
-                }
-                view = view?.superview
-            }
-            return nil
-        }
-
-        private func findNavBarInHierarchy(_ view: UIView) -> UINavigationBar? {
-            if let navBar = view as? UINavigationBar {
-                return navBar
-            }
-            for subview in view.subviews {
-                if let navBar = subview as? UINavigationBar {
-                    return navBar
-                }
-                if let navBar = findNavBarInHierarchy(subview) {
-                    return navBar
-                }
-            }
-            return nil
-        }
-    }
-#endif
